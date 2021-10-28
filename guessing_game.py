4bcbc8fd1233fc008dd587568501c8185d8157a8
MarcosD <md@marcosduran.com>
2021-10-27 21:58:39 -0400

    Before try change

--- guessing_game.py
+++ guessing_game.py
@@ -18,9 +18,12 @@ def start_game():
 def play_game():
   attempts = 1
   secret = random.randint(1, 10)
-
   try:
     guess = int(input("Guess a number between 1 and 10:   "))
+  except ValueError:
+    print("That is an invalid entry. Please input only integers between 1 and 10.")
+    play_game()
+  else:
     while guess != secret:
       if guess > secret:
         print("The number is lower.")
@@ -36,30 +39,6 @@ def play_game():
       print("{} you guessed the correct number in {} tries".format(player, attempts))
       print("Game Over!")
   
-
-    
-
-
-
-"""Psuedo-code Hints
-    
-When the program starts, we want to:
-------------------------------------
-1. Display an intro/welcome message to the player.
-2. Store a random number as the answer/solution.
-3. Continuously prompt the player for a guess.
-  a. If the guess greater than the solution, display to the player "It's lower".
-  b. If the guess is less than the solution, display to the player "It's higher".
-
-4. Once the guess is correct, stop looping, inform the user they "Got it"
-      and show how many attempts it took them to get the correct number.
-5. Let the player know the game is ending, or something that indicates the game is over.
-
-( You can add more features/enhancements if you'd like to. )
-"""
-# write your code inside this function.
-
-
 
 # Kick off the program by calling the start_game function.
 start_game()
